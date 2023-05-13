6b0a76e429e22cf7eb6f1d70d13a372aa0a7ec44
nesas_fahmimaulanafadila <fahmifadila1933@gmail.com>
2023-05-13 22:46:50 +0700

    update unit test

--- src/test/java/fahmimf/pencatatan/buku/AppTest.java
+++ src/test/java/fahmimf/pencatatan/buku/AppTest.java
@@ -1,20 +1,33 @@
 package fahmimf.pencatatan.buku;
 
-// import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-// import org.junit.Test;
+import javax.sql.DataSource;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    // @Test
-    // public void shouldAnswerWithTrue()
-    // {
-    //     assertTrue( true );
-    // }
+import org.junit.jupiter.api.Test;
+
+import fahmimf.pencatatan.buku.repository.BookRepository;
+import fahmimf.pencatatan.buku.repository.impl.BookRepositoryImpl;
+import fahmimf.pencatatan.buku.service.BookService;
+import fahmimf.pencatatan.buku.service.impl.BookServiceImpl;
+import fahmimf.pencatatan.buku.util.DatabaseUtil;
+import fahmimf.pencatatan.buku.view.BookView;
+
+public class AppTest {
+
+    @Test
+    public void testApp() {
+        // Datasource
+        DataSource dataSource = DatabaseUtil.getDataSource();
+
+        // Set Datasource
+        BookRepository bookRepository = new BookRepositoryImpl(dataSource);
+        BookService bookService = new BookServiceImpl(bookRepository);
+
+        // View
+        BookView bookView = new BookView(bookService);
+
+        assertNotNull(bookView);
+    }
+
 }
